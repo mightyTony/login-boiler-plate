a5dbf2d6255ed78d12c9f5002a18e8b36895e9d0
Sungmin Ha <mightytonykr@gmail.com>
2024-10-26 23:51:39 +0900

    first commit

---
+++ src/main/java/tony/spring/loginboilerplate/LoginBoilerPlateApplication.java
@@ -0,0 +1,13 @@
+package tony.spring.loginboilerplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginBoilerPlateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoginBoilerPlateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tony/spring/loginboilerplate/LoginBoilerPlateApplicationTests.java
@@ -0,0 +1,13 @@
+package tony.spring.loginboilerplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginBoilerPlateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
